c3adabfc8b50077946066489261b070bf19ea068
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2022-12-30 12:44:19 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,5 +1,17 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+ const ans = s
+    .split("")
+    .map(function (v) {
+      return mappings[v] || [""];
+    })
+    .reduce(function (a, b) {
+      var combi = [];
+      for (var i = 0; i < a.length; i++)
+        for (var j = 0; j < b.length; j++) combi.push(a[i] + b[j]);
+      return combi;
+    });
+  return ans;
 }
 
 module.exports = letterCombinations;
+
